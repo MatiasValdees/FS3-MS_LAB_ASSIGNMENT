e0240e8549596706fd49b21e84e1f95fecaa6c8d
MatiasValdees <matias.valdesr3@gmail.com>
2025-12-14 02:11:35 -0300

    apply cors

---
+++ src/main/java/cl/duoc/ms_lab_assignment/infrastructure/dtos/analysis/AnalysisResponse.java
@@ -0,0 +1,19 @@
+package cl.duoc.ms_lab_assignment.infrastructure.dtos.analysis;
+
+
+import cl.duoc.ms_lab_assignment.domain.entities.AnalysisEntity;
+
+public record AnalysisResponse(
+        Long id,
+        String name,
+        String description
+
+) {
+    public static AnalysisResponse fromEntity (AnalysisEntity entity){
+        return new AnalysisResponse(
+                entity.getId(),
+                entity.getName(),
+                entity.getDescription()
+        );
+    }
+}
